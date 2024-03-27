173b6456fbad2668671e740a0cec017a0b820986
Karan Khatri <karankhatri535@gmail.com>
2024-03-27 17:40:34 +0530

    created header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
+import { Button, HStack } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <HStack p={'4'} shadow={'base'} bgColor={'blackAlpha.900'} >
+        <Button variant={'unstyled'} color={'white'} >
+            <Link to='/' >Home</Link>
+        </Button>
+        <Button variant={'unstyled'} color={'white'} >
+            <Link to='/exchange' >Exchange</Link>
+        </Button>
+        <Button variant={'unstyled'} color={'white'} >
+            <Link to='/coin' >Coins</Link>
+        </Button>
+    </HStack>
   )
 }
 
